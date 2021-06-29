226a0714da352f17fef5969ce6cb4a1c4b437bbe
Vladyslav Udovychenko <rafull6@gmail.com>
2021-06-29 22:13:16 +0200

    Add search for movies

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const modalContainer = document.getElementById('register-modal');
 const signUpButton = document.getElementById('sign-up');
 const closeButton = document.getElementById('close-modal');
 const moviesSection = document.querySelector('.movies-section');
+const searchBtn = document.querySelector('#search');
+const searchField = document.querySelector('.search-field');
+const nav = document.querySelector('.top-nav');
 
 signUpButton.addEventListener('click', function () {
-    console.log('awdawd');
     modalContainer.classList.add('visible');
 });
 
@@ -28,8 +30,9 @@ const createMovieElement = (movieData) => {
     return movieContainer;
 };
 
-const renderMovies = () => {
-    const movies = mockedMovies.results;
+const renderMovies = (movies) => {
+    moviesSection.innerHTML = '';
+    // const movies = filteredMovies || mockedMovies.results;
 
     movies.forEach((movie) => {
         const movieElement = createMovieElement(movie);
@@ -37,4 +40,39 @@ const renderMovies = () => {
     });
 };
 
-renderMovies();
+let isSearchVisibile = false;
+
+searchBtn.addEventListener('click', () => {
+    isSearchVisibile = !isSearchVisibile;
+
+    if (isSearchVisibile === true) {
+        searchField.style.display = 'block';
+        nav.style.display = 'none';
+    } else {
+        searchField.style.display = 'none';
+        nav.style.display = 'block';
+    }
+});
+
+searchField.addEventListener('input', (event) => {
+    const text = event.target.value.toLowerCase();
+
+    const filteredMovies = mockedMovies.results.filter((movie) => {
+        return movie.original_title.toLowerCase().includes(text);
+    });
+
+    renderMovies(filteredMovies);
+});
+
+const url =
+    'https://api.themoviedb.org/3/movie/popular?api_key=a5be41cb9776230a9edbf288fc44483d';
+
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        console.log('response', response);
+    });
+
+renderMovies(mockedMovies.results);
